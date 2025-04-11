eeaeabae3217043b23bb1e5b070a08dc96402687
QuoteNat <35079138+QuoteNat@users.noreply.github.com>
2025-04-11 13:46:06 -0500

    Add searchButton event listener

--- src/index.js
+++ src/index.js
@@ -3,7 +3,17 @@ import "./app.js";
 import getWeatherData from "./app.js";
 console.log("Hello world!");
 
+const searchBar = document.getElementById("search");
+const searchButton = document.getElementById("search-button");
+
 let testString = "New York City, New York";
 getWeatherData(testString).then(function (result) {
   console.log(result);
+});
+
+searchButton.addEventListener("click", () => {
+  console.log("Searching for " + searchBar.value);
+  getWeatherData(searchBar.value).then((result) => {
+    console.log(result);
+  });
 });
